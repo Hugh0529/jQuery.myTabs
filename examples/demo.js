121c09fe9387de3bca86f925bd8b86a118cd28cd
chenhaoyang <chenhaoyang0529@gmail.com>
2015-08-19 19:47:38 +0800

    basic function

---
+++ examples/demo.js
@@ -0,0 +1,9 @@
+/**
+ * Created by chy on 15-8-19.
+ */
+$(document).ready( function() {
+    var setting = {};
+    setting.transitionIn = "fadeIn";
+    setting.transitionOut = "fadeOut";
+    $('#tab-container').myTabs(setting);
+});

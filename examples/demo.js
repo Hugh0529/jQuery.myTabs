9f7e0ed70cdb417ff7e0b35f5293eb943b89efc5
chenhaoyang <chenhaoyang0529@gmail.com>
2015-10-20 11:50:38 +0800

    optimize code style

--- examples/demo.js
+++ examples/demo.js
@@ -1,9 +1,6 @@
-/**
- * Created by chy on 15-8-19.
- */
 $(document).ready( function() {
     var setting = {};
-    setting.transitionIn = "fadeIn";
-    setting.transitionOut = "fadeOut";
+    setting.transitionIn = 'fadeIn';
+    setting.transitionOut = 'fadeOut';
     $('#tab-container').myTabs(setting);
 });
